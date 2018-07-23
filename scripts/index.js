ff111938a6ac03a21324a4d478bca942e4abcddd
Keith Dahlby <keith@newbo.co>
2018-07-23 12:03:44 -0500

    Add plain HTML with basic JS environment

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+'use strict';
+var app = app || {};
+
+(function (module) {
+
+  let productionApiUrl = 'https://cr-301-2018-todo-app.herokuapp.com/';
+  let developmentApiUrl = 'http://localhost:3000';
+
+  module.isProduction = /^(?!localhost|127)/.test(window.location.hostname);
+
+  module.ENVIRONMENT = {
+    apiUrl: module.isProduction ? productionApiUrl : developmentApiUrl
+  };
+
+  console.log(module.ENVIRONMENT);
+})(app);
